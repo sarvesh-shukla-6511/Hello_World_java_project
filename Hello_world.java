92eeb08948fef56b9ae2dffa81b799638b79727e
Sarvesh shukla <MY_NAME@example.com>
2022-11-21 23:44:58 +0530

    add the changes in java

--- Hello_world.java
+++ Hello_world.java
@@ -5,8 +5,6 @@ public class Hello_world {
 
     public static void main(String[] args) {
         System.out.println("Welcome to java Hello World project");
-        System.out.println("this is line 2");
-        System.out.println("this is third line");
 
     }
 }
